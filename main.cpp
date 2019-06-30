33e1c5336c91220705972b880200864fd7c79bff
AustenBrooks <austenbrooks1@gmail.com>
2019-06-30 21:11:33 +0000

    reworked spacing and choice input

--- main.cpp
+++ main.cpp
@@ -31,8 +31,10 @@ int main(){
         
         while(isBetting){
             bet=0;
+            
             cout << "You have $" <<playerCash<<". How much would you like to bet?"<<endl;
             cin>>bet;
+            
             if(bet>playerCash){
                 cout<<"You do not have enough money for that bet."<<endl;
             }
@@ -60,10 +62,12 @@ int main(){
         int turn=1;
         
         dealerScore = dealer.at(0)->getValue()+dealer.at(1)->getValue();
+        
         if(dealerScore==21){
             isDrawing = false;
             playerScore = player.at(0)->getValue()+player.at(1)->getValue();
         }
+        
         while(isDrawing){
             cout<<"Dealer's Hand"<<endl;
             renderDealer(dealer);
@@ -71,30 +75,34 @@ int main(){
             cout<<"Player's Hand"<<endl;
             renderHand(player);
             
+            string choice;
             
-            string choice="";
-            
-            if(turn==1){
-                cout<<"Would you like to: hit, stay, or double down?"<<endl;
-            }
-            else
-                cout<<"Would you like to: hit or stay?"<<endl;
-            
-            if(turn==1){
-                cin.ignore();
+            while(1){
+                choice="";
+                cin.clear();
+                if(turn==1)
+                    cout<<"Would you like to: hit, stay, or double down?"<<endl;
+                
+                else
+                    cout<<"Would you like to: hit or stay?"<<endl;
+                
                 getline(cin, choice);
+                
+                lowercaseString(choice);
+                
+                if(choice=="hit"||choice=="stay"||choice=="double down"){
+                    cout<<"K:"<<endl;
+                    break;
+                }
             }
-            else
-                cin>>choice;
-            
-            lowercaseString(choice);
-            
             if(choice=="hit"){
                 draw(player, deck, discard, 1);
             }
+            
             else if(choice=="stay"){
                 isDrawing=false;
             }
+            
             else if(choice=="double down"&&turn==1){
                 if(playerCash>bet){
                     playerCash-=bet;
@@ -107,16 +115,19 @@ int main(){
             }
             playerScore=0;
             int numAces=0;
+            
             for(int i=0; i<player.size(); ++i){
                 if(player.at(i)->getValue()==11){
                     ++numAces;
                 }
                 playerScore+=player.at(i)->getValue();
             }
+            
             while(numAces>0&&playerScore>21){
                 playerScore-=10;
                 --numAces;
             }
+            
             if(playerScore>21){
                 cout<<"You busted"<<endl;
                 isDrawing=false;
@@ -136,11 +147,13 @@ int main(){
                 }
                 dealerScore+=dealer.at(i)->getValue();
             }
+            
             while(numAces>0&&dealerScore>21){
                 dealerScore-=10;
                 numAces--;
             }
         }
+        
         if(playerScore==21&&player.size()==2)
             isDrawing=false;
         
@@ -154,20 +167,25 @@ int main(){
                 }
                 dealerScore+=dealer.at(i)->getValue();
             }
+            
             while(numAces>0&&dealerScore>21){
                 dealerScore-=10;
                 numAces--;
             }
+            
             if(dealerScore>21){
                 cout<<"Dealer busts!"<<endl;
                 isDrawing=false;
                 dealerScore=0;
             }
+            
             else if(dealerScore<17)
                 draw(dealer, deck, discard, 1);
+            
             else if(dealerScore>17)
                 isDrawing=false;
         }
+        
         cout<<"Dealer's Hand"<<endl;
         renderHand(dealer);
         
@@ -175,13 +193,16 @@ int main(){
         renderHand(player);
         
         cout<<"Dealer got "<<dealerScore<<". You got "<<playerScore<<endl;
+        
         if(dealerScore==21&&dealer.size()==2){
             cout<<"Dealer got blackjack, you lose"<<endl;
         }
+        
         else if(playerScore==21&&player.size()==2){
             cout<<"You got blackjack, you win double your bet!"<<endl;
             playerCash+=bet*3;
         }
+        
         else if(playerScore>dealerScore){
             cout<<"You won! You made $"<< bet<<endl;
             playerCash+= bet*2;
